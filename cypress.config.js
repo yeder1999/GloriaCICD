f3e13e4d7f58e7915e57d19c881d41c5e14d5c38
yeder1999 <benamaryedir1999@gmail.com>
2026-08-30 15:26:02 +0200

    refactor: update Cypress configuration to use ES modules and integrate Allure reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,19 @@
-const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
+import { defineConfig } from "cypress";
+import { allureCypress } from "allure-cypress/reporter";
+
+export default defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+
+      return config;
     },
   },
 });
+
